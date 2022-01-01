31e8cd47fad514437d6c258a51f87bdd33be4090
Mustakmia <musyafijur52@gmail.com>
2022-01-02 00:47:41 +0600

    firebase addinge

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA6y_G9rQF4dSx3DALSJqJ07APT4GrvIQs",
+    authDomain: "daily-shop-3c3c2.firebaseapp.com",
+    projectId: "daily-shop-3c3c2",
+    storageBucket: "daily-shop-3c3c2.appspot.com",
+    messagingSenderId: "126600336973",
+    appId: "1:126600336973:web:8ca0e4f1b7473470bf2e04",
+    measurementId: "G-B9J0QPMCB3"
+};
+
+export default firebaseConfig
