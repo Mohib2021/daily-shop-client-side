b582244ab2fd6012c34bd25cae8c3269dbb56a74
Mustakmia <musyafijur52@gmail.com>
2022-01-02 01:02:18 +0600

    router sete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./components/pages/Login/Login"
 
 function App() {
 	return (
-		<div className="App">
-			<h2>Hello </h2>
-		</div>
+		<BrowserRouter>
+			<div>
+				<h1>Hello, React Router!</h1>
+				<Routes>
+					<Route path="/" element={<Login />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
 
