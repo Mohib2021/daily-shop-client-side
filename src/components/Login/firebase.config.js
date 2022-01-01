194ef9f3e5bbff72c5763182caa123e374bceda1
Mustakmia <musyafijur52@gmail.com>
2022-01-02 01:23:41 +0600

    firebase moddifye

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJqZOahRktkNiPWK-ILsfuzuyL7yDxgBU",
+  authDomain: "food-delivery-f500a.firebaseapp.com",
+  projectId: "food-delivery-f500a",
+  storageBucket: "food-delivery-f500a.appspot.com",
+  messagingSenderId: "498862988608",
+  appId: "1:498862988608:web:4c47fc5e5e173a031a2ea5",
+  measurementId: "G-YQ4ZSFGY8K"
+};
+
+export default firebaseConfig;
