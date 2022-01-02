910db7e61ebf36ff934b872b829478fef449a1b2
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-01-03 01:36:17 +0600

    add firebase

---
+++ src/components/Firebase/Firebase.init.js
@@ -0,0 +1,6 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./Firebase.config";
+const firebaseInitialization = () => {
+	initializeApp(firebaseConfig);
+};
+export default firebaseInitialization;
