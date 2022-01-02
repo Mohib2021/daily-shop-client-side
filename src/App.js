9e2f6b7fba8b0a8910f65198203ad966763d35b1
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-01-02 17:57:57 +0600

    add route and home page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-
+import Home from "./components/Pages/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
 	return (
 		<div className="App">
-			<h2>Hello </h2>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/home" element={<Home />} />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
