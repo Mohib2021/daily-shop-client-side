560877338d8066dbab86dbbc0f84695e84ef3ee0
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-01-03 03:31:30 +0600

    add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
-import Home from './components/Pages/Home/Home';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Home from "./components/Pages/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Pages/Login/Login";
+import Register from "./components/Pages/Register/Register";
+import NotFound from "./components/Pages/404/NotFound";
 function App() {
 	return (
-		<div className="App">
-			<Home></Home>
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</Router>
 	);
 }
- 
+
 export default App;
