daf5f626f8321f735f65c608792f025745ff7e5c
Mustakmia <musyafijur52@gmail.com>
2022-01-03 00:37:16 +0600

    matarial ui dashboard creating

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Login from "./components/pages/Login/Login"
+
 
 function App() {
 	return (
-		<BrowserRouter>
-			<div>
-				<h1>Hello, React Router!</h1>
-				<Routes>
-					<Route path="/" element={<Login />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
+		<h1>Hellow</h1>
 	);
 }
 
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Login = () => {
+const Dashboard = () => {
     return (
         <div>
-            <h1>login</h1>
+
         </div>
     );
 };
 
-export default Login;
+export default Dashboard;
