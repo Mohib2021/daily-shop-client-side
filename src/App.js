7554ee7396836faedf7078f622e174bba0d7825b
Mustakmia <musyafijur52@gmail.com>
2022-01-04 00:26:45 +0600

    firebase completeg

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 function App() {
 	return (
-		<h1>Hellow</h1>
+		<div>hhh</div>
 	);
 }
 
---
+++ src/components/Firebase/Firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9VBQRK9TM8ZpnM3etWTV0WiJ4WV6z7qE",
+    authDomain: "world-tour-b7fb6.firebaseapp.com",
+    projectId: "world-tour-b7fb6",
+    storageBucket: "world-tour-b7fb6.appspot.com",
+    messagingSenderId: "3856074309",
+    appId: "1:3856074309:web:4605c4a4450e5266b4052c",
+    measurementId: "G-YP36EMB0XL"
+};
+
+export default firebaseConfig
